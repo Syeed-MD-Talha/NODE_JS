e19f3e3bf2462494cdd6916fe447157d627a9d7c
Syeed MD Talha <syeedmdtalha@gmail.com>
2022-08-02 15:26:12 +0600

    Node js

---
+++ Lesson1/student.js
@@ -0,0 +1,18 @@
+
+function person()
+{
+    return "My name is Talha";
+}
+
+const age=()=>
+{
+    return "I'm 22 years old";
+}
+
+let cat="I have 2 kitten";
+
+module.exports={
+    person,
+    age,
+    cat,
+}
